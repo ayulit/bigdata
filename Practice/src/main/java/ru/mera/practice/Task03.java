67769bfdb725e90feeede43ce061f8af40055c59
ayulit <litwinoff@gmail.com>
2017-05-19 18:16:45 +0300

    Practice: Task03: Using snappy compression

--- Practice/src/main/java/ru/mera/practice/Task03.java
+++ Practice/src/main/java/ru/mera/practice/Task03.java
@@ -1,7 +1,5 @@
 package ru.mera.practice;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -14,21 +12,18 @@ public class Task03 {
 
     public static void main(String[] args) {
 
-        String appName = Task03.class.getSimpleName();
-
         // Should be some file on your system in 'resources'
         String file = "src/main/resources/people.json";
-        
-        // Initializing Spark
-        SparkConf conf = new SparkConf().setAppName(appName).setMaster("local[4]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
         
         // The entry point into Spark SQL functionality
         SparkSession spark = SparkSession
                 .builder()
-                .appName(appName)
+                .master("local")
                 .getOrCreate();
 
+        // Configuration to use snappy compression
+        spark.conf().set("spark.sql.avro.compression", "snappy");        
+        
         // Creating DataFrame from avro
 //        Dataset<Row> df = spark.read().format("com.databricks.spark.avro").load("src/main/resources/autos.avro");        
         
@@ -37,8 +32,8 @@ public class Task03 {
         
         // Displays the content of the DataFrame to stdout
         df.show(); 
-
-        // Saving to avro
+        
+        // Writing out to avro
         df.write().format("com.databricks.spark.avro").save("output/");
         
         System.out.println("Done.");
