79d54d9bdd86f1178237580593d93b1b6f006992
alitvin <litwinoff@gmail.com>
2017-05-19 15:02:46 +0300

    Practice: Task03: Reading from *.avro to DataFrame

---
+++ Practice/src/main/java/ru/mera/practice/Task03.java
@@ -0,0 +1,40 @@
+package ru.mera.practice;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * Create table in *.avro using snappy codec
+ * 
+ * */
+public class Task03 {
+
+    public static void main(String[] args) {
+
+        String appName = Task03.class.getSimpleName();
+
+        // Should be some file on your system in 'resources'
+        String file = "src/main/resources/people.json";
+        
+        // Initializing Spark
+        SparkConf conf = new SparkConf().setAppName(appName).setMaster("local[4]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        
+        // The entry point into Spark SQL functionality
+        SparkSession spark = SparkSession
+                .builder()
+                .appName(appName)
+                .getOrCreate();
+
+        // Creating DataFrame from avro
+        Dataset<Row> df = spark.read().format("com.databricks.spark.avro").load("src/main/resources/autos.avro");        
+        
+        // Displays the content of the DataFrame to stdout
+        df.show(); 
+        
+    }
+
+}
